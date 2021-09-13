ab7c9f84fb1628086dabf9e0e0f99c529b4dbe64
Retno Ayu Adiati <retno_ayu@hotmail.com>
2021-09-13 17:00:52 +0200

    Updates

---
+++ precache-manifest.bf007d4a0141e868041c8c1053dcfc4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df4b1ed690fef5613c34ab5760c6a86f",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "a5ee89ea28f9c8752659",
+    "url": "/react-todo-app/static/css/main.d0e72eea.chunk.css"
+  },
+  {
+    "revision": "1ed380a66c2651c5f79b",
+    "url": "/react-todo-app/static/js/2.d2bc8f71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.d2bc8f71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5ee89ea28f9c8752659",
+    "url": "/react-todo-app/static/js/main.07a63953.chunk.js"
+  },
+  {
+    "revision": "1528fb5b7daed31266f4",
+    "url": "/react-todo-app/static/js/runtime-main.1dd0cb46.js"
+  }
+]);
