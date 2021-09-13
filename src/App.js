e678587bc4c5cd77c23419c342c408cdebe7297b
Retno Ayu Adiati <retno_ayu@hotmail.com>
2021-09-13 16:28:22 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Header from './components/Header'
+import TodoItem from './components/TodoItem'
+import todosData from './components/todosData'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			todos: todosData,
+		}
+		this.handleChange = this.handleChange.bind(this)
+	}
+
+  handleChange(id) {
+    const updateTodos = this.state.todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    this.setState({
+      todos: updateTodos
+    })
+  }
+
+	render() {
+		const todoItems = this.state.todos.map((item) => (
+			<TodoItem key={item.id} item={item} handleChange={this.handleChange} />
+		))
+
+		return (
+      <div className='todo-list'>
+        <Header /> 
+        {todoItems}
+      </div>)
+	}
 }
 
-export default App;
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="header">
+      <header>
+        <h1>Todo List</h1>
+      </header>
+    </div>
+  )
+}
+
+export default Header
